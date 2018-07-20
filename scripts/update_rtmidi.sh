611f4a7310fa57d97294b299bcc06dc89dc3bc2a
danomatika <danomatika@gmail.com>
2018-07-21 00:15:16 +0200

    rtmidi 3.0.0

--- scripts/update_rtmidi.sh
+++ scripts/update_rtmidi.sh
@@ -12,7 +12,7 @@
 
 WD=$(dirname $0)
 
-VER=2.1.1
+VER=3.0.0
 
 SRC=rtmidi-$VER
 DEST=../libs/rtmidi
@@ -32,7 +32,7 @@ tar -xvf rtmidi-$VER.tar.gz
 mkdir -p $DEST
 
 # copy license
-cp -v $SRC/readme $DEST
+cp -v $SRC/README.md $DEST
 
 # don't need C API
 rm $SRC/rtmidi_c.*
